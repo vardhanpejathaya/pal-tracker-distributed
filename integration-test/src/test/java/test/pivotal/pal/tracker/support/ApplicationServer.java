8e409895dec18946c1f1d7153c47526c305b4c03
vardhan <vardhan.56@wipro.com>
2020-02-12 23:25:02 -0800

    Lab:1 Deploying a Distributed System

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -53,7 +53,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 120;
+        int timeout = 120000000;
         Instant start = Instant.now();
         boolean isUp = false;
 
